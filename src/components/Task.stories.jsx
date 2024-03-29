18ee47f28ddae59d91c060fd93e6bfd5a6ab77a9
Yanina Kudina <yaninkaolegovna@gmail.com>
2024-03-29 15:28:26 +0300

    Story AllStates

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -1,10 +1,10 @@
-
 import Task from './Task';
 
 export default {
   component: Task,
   title: 'Task',
   tags: ['autodocs'],
+  decorators: [(Story) => <div style={{ display: 'flex', flexDirection: 'column' }}><Story /></div>],
 };
 
 export const Default = {
@@ -15,6 +15,9 @@ export const Default = {
       state: 'TASK_INBOX',
     },
   },
+  story: {
+    name: 'Default',
+  },
 };
 
 export const Pinned = {
@@ -23,6 +26,9 @@ export const Pinned = {
       ...Default.args.task,
       state: 'TASK_PINNED',
     },
+  },
+  story: {
+    name: 'Pinned',
   },
 };
 
@@ -33,4 +39,40 @@ export const Archived = {
       state: 'TASK_ARCHIVED',
     },
   },
+  story: {
+    name: 'Archived',
+  },
 };
+
+export const AllStates = () => (
+  <>
+    <Label>Default</Label>
+    <DefaultComponent {...Default.args} />
+
+    <StyledHr />
+
+    <StyledLabel>Pinned</StyledLabel>
+    <PinnedComponent {...Pinned.args} />
+
+    <StyledHr />
+
+    <StyledLabel>Archived</StyledLabel>
+    <ArchivedComponent {...Archived.args} />
+  </>
+);
+
+const DefaultComponent = (args) => <Task {...args} />;
+const PinnedComponent = (args) => <Task {...args} />;
+const ArchivedComponent = (args) => <Task {...args} />;
+
+const Label = ({ children }) => (
+  <h3 style={{ margin: '10px 0', color: 'white' }}>{children}</h3>
+);
+
+const StyledLabel = ({ children }) => (
+  <h3 style={{ marginTop: '20px', marginBottom: '10px', color: 'white' }}>{children}</h3>
+);
+
+const StyledHr = () => (
+  <hr style={{ margin: '10px 0', height: '0', border: 'none', borderTop: '2px dashed #fff' }} />
+);
